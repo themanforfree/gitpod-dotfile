560d424d229a608799ce154f35a3dd33c2c2324b
themanforfree <56149350+themanforfree@users.noreply.github.com>
2022-08-03 11:50:08 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-sudo apt update && sudo apt install -y neofetch
+sudo apt update && sudo apt install -y neofetch libprotobuf-dev protobuf-compiler
 
 wget https://github.com/GitCredentialManager/git-credential-manager/releases/download/v2.0.785/gcm-linux_amd64.2.0.785.deb
 sudo dpkg -i gcm-linux_amd64.2.0.785.deb
