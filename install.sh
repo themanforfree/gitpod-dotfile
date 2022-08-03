6b21d90353b7b029f3e081881cd2a24e7cd9f5eb
themanforfree <56149350+themanforfree@users.noreply.github.com>
2022-08-03 11:36:26 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-sudo apt update && sudo apt install neofetch
+sudo apt update && sudo apt install -y neofetch
 
 wget https://github.com/GitCredentialManager/git-credential-manager/releases/download/v2.0.785/gcm-linux_amd64.2.0.785.deb
 sudo dpkg -i gcm-linux_amd64.2.0.785.deb
