2a0c4151b678eca857f2612f6ae9f7327bbef473
themanforfree <themanforfree@gmail.com>
2022-08-26 11:02:58 +0800

    codespace

--- install.sh
+++ install.sh
@@ -1,19 +1,6 @@
-sudo apt update && sudo apt install -y neofetch libprotobuf-dev protobuf-compiler
+which cargo
+if [$? -ne 0]; then
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+fi
 
-wget https://github.com/GitCredentialManager/git-credential-manager/releases/download/v2.0.785/gcm-linux_amd64.2.0.785.deb
-sudo dpkg -i gcm-linux_amd64.2.0.785.deb
-rm gcm-linux_amd64.2.0.785.deb
-git-credential-manager configure
-
-gitpod-code --install-extension christian-kohler.path-intellisense
-gitpod-code --install-extension eamodio.gitlens
-gitpod-code --install-extension esbenp.prettier-vscode
-gitpod-code --install-extension GitHub.copilot
-gitpod-code --install-extension Gruntfuggly.todo-tree
-gitpod-code --install-extension mhutchie.git-graph
-gitpod-code --install-extension ms-vscode.cpptools
-gitpod-code --install-extension rust-lang.rust-analyzer
-gitpod-code --install-extension serayuzgur.crates
-gitpod-code --install-extension tamasfe.even-better-toml
-gitpod-code --install-extension usernamehw.errorlens
-gitpod-code --install-extension vadimcn.vscode-lldb
+sudo apt update && sudo apt install -y neofetch && sudo upgrade -y
