63111a91ecc4e92a0f55e89a8e809cb77864e6a8
themanforfree <themanforfree@gmail.com>
2022-08-26 11:08:01 +0800

    fix

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@ if [$? -ne 0]; then
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 fi
 
-sudo apt update && sudo apt install -y neofetch && sudo upgrade -y
+sudo apt update && sudo apt install -y neofetch && sudo apt upgrade -y
